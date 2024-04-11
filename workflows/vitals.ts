91f51773a4275d8e1dba834c1292e56de7ae7825
François Vaux <madx+github@yapok.org>
2024-04-11 22:40:43 +0200

    fix(workflows/vitals): Extract LOG_SCOPE

--- workflows/vitals.ts
+++ workflows/vitals.ts
@@ -1,16 +1,20 @@
 import { Bot } from "../bot";
 import { pick } from "../utils";
 
+const LOG_SCOPE = "workflow/vitals";
+
 export default async function vitals(bot: Bot) {
   // Make sure the deploy command is available
   const deployCommand = bot.commands.get("deploy");
 
   if (!deployCommand) {
-    bot.logger.fatal("workflow/vitals", "Unable to find the deploy command");
+    bot.logger.fatal(LOG_SCOPE, "Unable to find the deploy command");
   }
 
   await bot.guild.commands.create({
     ...pick(deployCommand, ["name", "description"]),
     defaultMemberPermissions: "0",
   });
+
+  bot.logger.info(LOG_SCOPE, "Registered deploy command");
 }
